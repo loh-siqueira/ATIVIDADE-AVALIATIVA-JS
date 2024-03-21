e99816d81dc5a1b050d087c592b2303dfc2a00ee
loh-siqueira <128156957+loh-siqueira@users.noreply.github.com>
2024-03-21 08:58:11 -0300

    Add files via upload

---
+++ loh.js
@@ -0,0 +1,9 @@
+var nota = 8;
+ if(nota>6){
+    console.log('você esta aprovado')
+ }else if (nota=6){
+    console.log('você esta de recuperação')
+ }else{
+    console.log('você esta reprovado')
+ }
+
